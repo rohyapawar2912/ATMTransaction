9289d516836a5a01eac502a589a8271f359f5dc4
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-07-11 09:51:43 +0530

    [Rohit]Add: // add all files

---
+++ ATMTransaction/ATMTransaction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    public class ATMTransaction
+    {
+        private decimal balance;
+        public ATMTransaction(decimal initialbalance)
+        {
+            balance = initialbalance;
+        }
+
+        public void Deposite(decimal amount)
+        {
+            balance += amount;
+            Console.WriteLine($"Deposited :{amount:c}");
+            Console.WriteLine($"now balance : {balance:c}");
+        }
+        public void Withdraw(decimal amount)
+        {
+            if(amount > balance)
+            {
+                Console.WriteLine("Insufficient balance");
+                return;
+            }
+            balance -= amount;
+            Console.WriteLine($"Withdraw : {amount :c}");
+            Console.WriteLine($"now balance : {balance :c}");
+        }
+        public void Displaybalance()
+        {
+            Console.WriteLine($"Current Balance : {balance :c}");
+
+        }
+    }
+}
---
+++ ATMTransaction/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal initialBalance = 6000;
+            ATMTransaction atm = new ATMTransaction(initialBalance);
+
+            while (true)
+            {
+                Console.WriteLine("Please select below option :");
+                Console.WriteLine("1 - Deposit");
+                Console.WriteLine("2 - Withdraw");
+                Console.WriteLine("3 - check balance");
+                Console.WriteLine("4 - exit ");
+                Console.WriteLine("--------------------------------------------------");
+                int choise = Convert.ToInt32(Console.ReadLine());
+
+                switch (choise)
+                {
+                    case 1:
+                        Console.WriteLine(" Deposit amount: ");
+                        decimal depositAmount = Convert.ToInt32(Console.ReadLine());
+                        atm.Deposite(depositAmount);
+                        break;
+                    case 2:
+                        Console.WriteLine("Withdraw amount : ");
+                        decimal withdrawAmount = Convert.ToInt32(Console.ReadLine());
+                        atm.Withdraw(withdrawAmount);
+                        break;
+                    case 3:
+                        atm.Displaybalance();
+                        break;
+                    case 4:
+                        Console.WriteLine("Thank you for using ATM . Good Bye");
+                        return;
+                    default:
+                        Console.WriteLine("invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
